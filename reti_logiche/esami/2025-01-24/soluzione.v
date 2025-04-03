30121bcc67c65ef3e5a1764e990c52f19de4bfd8
matteofortino <matteofortino04@gmail.com>
2025-04-03 14:07:33 +0200

    aggiunta solizione del prof

--- reti_logiche/esami/2025-01-24/soluzione.v
+++ reti_logiche/esami/2025-01-24/soluzione.v
@@ -19,8 +19,8 @@ module ABC (
     
     reg [7:0] COUNT;
 
-    reg [2:0] STAR;
-    localparam S0=0, S1=1, S2=2, S3=3, S4=4; 
+    reg [1:0] STAR;
+    localparam S0=0, S1=1, S2=2, S3=3; 
 
     wire [7:0] m;
     MEDIA me (
